a7e94691151e00ef27305b98b1d0d8769cd60bb1
dchenkpmg <dylanchen1@kpmg.co.nz>
2025-03-07 16:21:34 +1300

    feat: update template

--- src/utils.js
+++
@@ -1,7 +0,0 @@
-export function createElement(tag, textContent, parent, className) {
-  const element = document.createElement(tag);
-  if (textContent) element.textContent = textContent;
-  if (className) element.className = className;
-  parent.appendChild(element);
-  return element;
-}
